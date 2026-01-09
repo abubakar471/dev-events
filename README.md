39663a326e823114e6e86b1841f22d62840cc0e1
AB <87719025+abubakar471@users.noreply.github.com>
2026-01-09 14:47:16 +0600

    implement posthog

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # (frontend) hero rays
 hero rays was used using react bits component library.
+
+# (frontend) implementing posthog for analytics
+implement posthog right after creating the first component may be the hero. so that you can develop and track from the very first moment
+
+npx -y @posthog/wizard@latest
+
+then create a providers.tsx in the app directory
+
+and then wrap the root layout.tsx file with the provider
+
+you can also manually trigger events
+
+Optional: Send a manual event
+If you'd like, you can manually define events, too.
+
+posthog.capture('my event', { property: 'value' })
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
 import LightRays from "@/components/landing/light-rays/LightRays";
+import { PostHogProvider } from './providers'
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-Schibsted-grotesk",
@@ -28,24 +29,26 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} antialiased min-h-screen`}
       >
-        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
-          <LightRays
-            raysOrigin="top-center-offset"
-            raysColor="#5dfeca"
-            raysSpeed={0.5}
-            lightSpread={0.9}
-            rayLength={1.4}
-            followMouse={true}
-            mouseInfluence={0.1}
-            noiseAmount={0.0}
-            distortion={0.01}
-            className="custom-rays"
-          />
-        </div>
+        <PostHogProvider>
+          <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+            <LightRays
+              raysOrigin="top-center-offset"
+              raysColor="#5dfeca"
+              raysSpeed={0.5}
+              lightSpread={0.9}
+              rayLength={1.4}
+              followMouse={true}
+              mouseInfluence={0.1}
+              noiseAmount={0.0}
+              distortion={0.01}
+              className="custom-rays"
+            />
+          </div>
 
-        <main>
-          {children}
-        </main>
+          <main>
+            {children}
+          </main>
+        </PostHogProvider>
       </body>
     </html>
   );
